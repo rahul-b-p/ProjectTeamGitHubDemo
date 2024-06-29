b9b559c09439188b1ef6ef1a1977ce8e0e34b6a5
Rahul-BP <rahulzantoz10@lbscek.ac.in>
2024-06-29 15:50:30 +0530

    created and component Rahul added

---
+++ src/pages/Rahul.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Rahul() {
+  return (
+    <div style={{width:"100%", display:"flex", alignItems:"center", justifyContent:"center", backgroundColor:"red", padding:"5px"}}>
+        <p style={{color:"white", fontWeight:"bolder"}}>Rahul B P</p>
+    </div>
+  )
+}
+
+export default Rahul
